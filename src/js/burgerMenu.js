bb255a4d1ab8ca5101de23326c669bc8810715e3
Ypags <oartem.28082007@gmail.com>
2025-03-23 20:05:23 +0700

    Добавлены страницы Home, Products, Contact, FAQ. Сделано бургер меню. Добавлен лист с часами. Чуть чуть изменены структура проекта

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,3 +1,4 @@
-document.querySelector('.burger-menu').addEventListener('click', function () {
-	this.classList.toggle('active')
+document.querySelector('.burger-menu__span').addEventListener('click', function () {
+	document.querySelector('.burger-menu').classList.toggle('active__menu')
+	this.classList.toggle('active__span')
 })
